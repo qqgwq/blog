ecd3b9f17c66084f6c9a59b2f1dc03a4183993b5
gwq <1305851004@qq.com>
2019-02-28 14:43:30 +0800

    add gravatar

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,6 +5,7 @@
 @section('content')
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
+      <h3 class="comment-title"><span class="glyphicon glyphicon-comment"></span>{{ $post->comments->count() }}  Comments</h3>
       <h1>{{ $post->title }}</h1>
       <p>{{ $post->body }}</p>
       <hr>
@@ -16,8 +17,16 @@
     <div class="col-md-8 col-md-offset-2">
       @foreach($post->comments as $comment)
         <div class="comment">
-          <p><strong>Name:</strong>{{ $comment->name }}</p>
-          <p><strong>Comment:</strong><br>{{ $comment->comment }}</p><br>
+          <div class="author-info">
+            <img src="{{ "https://www.gravatar.com/avatar/" . md5(strtolower(trim($comment->email))) . "?s=50&d=wavatar" }}" class="author-image">
+            <div class="author-name">
+              <h4>{{ $comment->name }}</h4>
+              <p class="author-time">{{date('Y-m-d', strtotime($comment->created_at)) }}</p>
+            </div>
+          </div>
+          <div class="comment-content">
+              {{ $comment->comment }}
+          </div>
         </div>
         
       @endforeach
@@ -49,3 +58,5 @@
     </div>
   </div>
 @endsection
+
+<scritp type="text"
